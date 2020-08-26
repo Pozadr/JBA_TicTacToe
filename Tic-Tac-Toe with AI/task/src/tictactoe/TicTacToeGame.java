6fd9db110d7aa34eaa5ba4b1519e8fe42ebf7f4c
Pozadr <adpozor@gmail.com>
2020-08-26 12:19:34 +0200

    UserMove rebuild

--- Tic-Tac-Toe with AI/task/src/tictactoe/TicTacToeGame.java
+++ Tic-Tac-Toe with AI/task/src/tictactoe/TicTacToeGame.java
@@ -10,20 +10,29 @@ public class TicTacToeGame {
 
     public void gameInitialization() {
         // game init validation
-        while(true) {
-            System.out.print("Enter cells: ");
-            String inputFromUser = getStringInputFromUser();
-            if (inputFromUser.length() == 9) {
-                fillInitMatrix(inputFromUser);
-                break;
-            } else {
-                System.out.println("Wrong input!");
-            }
-        }
+        fillInitMatrix("_________"); // empty board
         printMatrix();
     }
 
     public void runGame() {
+        while (true) {
+            userMove();
+            printMatrix();
+            if (checkWhoWonTheGame()) {
+                break;
+            }
+            AiMove();
+            printMatrix();
+            if (checkWhoWonTheGame()) {
+                break;
+            }
+        }
+    }
+    private void AiMove() {
+        System.out.println("Making move level easy");
+
+    }
+    private void userMove() {
         int xCoordinate;
         int yCoordinate;
 
@@ -40,28 +49,15 @@ public class TicTacToeGame {
                     if (xCoordinate >= 1 && xCoordinate <= 3 && yCoordinate >= 1 && yCoordinate <= 3 ) {
                         // if cell is empty
                         if (gameMatrix[3 - yCoordinate][xCoordinate - 1] == ' ') {
-                            /*
-                            check:
-                            Since the game always starts with X, if the number of X's and O's on the field is the same,
-                            the user should make a move with X, and if X's is one more than O's,
-                            then the user should make a move with O.
-                            */
-                            if (checkXMoreThanO()) {
-                                gameMatrix[3 - yCoordinate][xCoordinate - 1] = 'O';
-                            } else {
-                                gameMatrix[3 - yCoordinate][xCoordinate - 1] = 'X';
-                            }
-                            printMatrix();
-                            checkWhoWonTheGame();
-
+                            gameMatrix[3 - yCoordinate][xCoordinate - 1] = 'X';
                             break; // break while loop
                         } else {
-                            System.out.println("This cell is occupied! Choose another one!");
-                        /* Suppose the bottom left cell has the coordinates (1, 1)
-                        and the top right cell has the coordinates (3, 3) like in this table:
-                        (1, 3) (2, 3) (3, 3)
-                        (1, 2) (2, 2) (3, 2)
-                        (1, 1) (2, 1) (3, 1) */
+                        System.out.println("This cell is occupied! Choose another one!");
+                    /* Suppose the bottom left cell has the coordinates (1, 1)
+                    and the top right cell has the coordinates (3, 3) like in this table:
+                    (1, 3) (2, 3) (3, 3)
+                    (1, 2) (2, 2) (3, 2)
+                    (1, 1) (2, 1) (3, 1) */
                         }
                     } else {
                         System.out.println("Coordinates should be from 1 to 3!");
@@ -74,6 +70,7 @@ public class TicTacToeGame {
             }
         }
     }
+
     private boolean checkXMoreThanO() {
         int xCounter = 0;
         int oCounter = 0;
@@ -88,7 +85,7 @@ public class TicTacToeGame {
         }
         return xCounter > oCounter;
     }
-    private void checkWhoWonTheGame() {
+    private boolean checkWhoWonTheGame() {
         //Check if there are some 'O' or 'X' in line: 00 01 02 | 10 11 12 | 20 21 22 | 00 10 20 | 01 11 21 | 02 12 22
         // or to the cross: 00 11 22 | 02 11 20.
         if (       gameMatrix[0][0] == 'X' && gameMatrix[0][1] == 'X' && gameMatrix[0][2] == 'X'
@@ -100,6 +97,7 @@ public class TicTacToeGame {
                 || gameMatrix[0][0] == 'X' && gameMatrix[1][1] == 'X' && gameMatrix[2][2] == 'X'
                 || gameMatrix[0][2] == 'X' && gameMatrix[1][1] == 'X' && gameMatrix[2][0] == 'X') {
             System.out.println("X wins");
+            return true;
         } else if (gameMatrix[0][0] == 'O' && gameMatrix[0][1] == 'O' && gameMatrix[0][2] == 'O'
                 || gameMatrix[1][0] == 'O' && gameMatrix[1][1] == 'O' && gameMatrix[1][2] == 'O'
                 || gameMatrix[2][0] == 'O' && gameMatrix[2][1] == 'O' && gameMatrix[2][2] == 'O'
@@ -109,14 +107,13 @@ public class TicTacToeGame {
                 || gameMatrix[0][0] == 'O' && gameMatrix[1][1] == 'O' && gameMatrix[2][2] == 'O'
                 || gameMatrix[0][2] == 'O' && gameMatrix[1][1] == 'O' && gameMatrix[2][0] == 'O') {
             System.out.println("O wins");
-
+            return true;
         } else if (gameMatrix[0][0] != ' ' && gameMatrix[0][1] != ' ' && gameMatrix[0][2] != ' '
                 && gameMatrix[1][0] != ' ' && gameMatrix[1][1] != ' ' && gameMatrix[1][2] != ' '
                 && gameMatrix[2][0] != ' ' && gameMatrix[2][1] != ' ' && gameMatrix[2][2] != ' ') {
             System.out.println("Draw");
-        } else {
-            System.out.println("Game not finished");
         }
+        return false;
     }
 
     private String getStringInputFromUser() {
