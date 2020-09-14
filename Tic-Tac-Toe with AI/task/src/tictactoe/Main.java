8dfb39953d73d3080083b9c4b19dca3446d102b2
Pozadr <adpozor@gmail.com>
2020-09-14 11:08:33 +0200

    Optimization: Player -> createPlayer(method)

--- Tic-Tac-Toe with AI/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe with AI/task/src/tictactoe/Main.java
@@ -18,57 +18,15 @@ public class Main {
                     else {
                         String player1Input = input[1];
                         String player2Input = input[2];
-                        Player player1;
-                        Player player2;
                         // input validation
                         if ((player1Input.equals("user") || player1Input.equals("easy")
                                 || player1Input.equals("medium") || player1Input.equals("hard"))
                          && (player2Input.equals("user") || player2Input.equals("easy")
                                 || player2Input.equals("medium") || player2Input.equals("hard"))) {
                             // creating object of player1
-                            switch (player1Input) {
-                                case "user": {
-                                    player1 = new User(Symbol.X);
-                                    break;
-                                }
-                                case "easy": {
-                                    player1 = new AIEasy(Symbol.X);
-                                    break;
-                                }
-                                case "medium": {
-                                    player1 = new AIMedium(Symbol.X);
-                                    break;
-                                }
-                                case "hard": {
-                                    player1 = new AIHard(Symbol.X);
-                                    break;
-                                }
-                                default: {
-                                    player1 = new Player();
-                                }
-                            }
+                            Player player1 = Player.createPlayer(Symbol.X, player1Input);
                             // creating object of player2
-                            switch (player2Input) {
-                                case "user": {
-                                    player2 = new User(Symbol.O);
-                                    break;
-                                }
-                                case "easy": {
-                                    player2 = new AIEasy(Symbol.O);
-                                    break;
-                                }
-                                case "medium": {
-                                    player2 = new AIMedium(Symbol.O);
-                                    break;
-                                }
-                                case "hard": {
-                                    player2 = new AIHard(Symbol.O);
-                                    break;
-                                }
-                                default: {
-                                    player2 = new Player();
-                                }
-                            }
+                            Player player2 = Player.createPlayer(Symbol.O, player2Input);
                             // ---------------------- RUN GAME ----------------------
                             runGame(player1, player2);
                             // ----------------------          ----------------------
@@ -92,7 +50,7 @@ public class Main {
         }
     }
 
-    public static void runGame (Player p1, Player p2) {
+    private static void runGame (Player p1, Player p2) {
         GameMatrix matrix = new GameMatrix();
         matrix.printMatrix(); // init empty board | later main menu
         while (true) {
--- Tic-Tac-Toe with AI/task/src/tictactoe/Player.java
+++ Tic-Tac-Toe with AI/task/src/tictactoe/Player.java
@@ -5,4 +5,24 @@ public class Player {
     protected void move(GameMatrix matrix) {
 
     }
+
+    public static Player createPlayer (Symbol symbol, String request){
+        switch (request) {
+            case "user": {
+                return new User(symbol);
+            }
+            case "easy": {
+                return  new AIEasy(symbol);
+            }
+            case "medium": {
+                return  new AIMedium(symbol);
+            }
+            case "hard": {
+                return  new AIHard(symbol);
+            }
+            default: {
+                return  new Player();
+            }
+        }
+    }
 }
